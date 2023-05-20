92c85f1662f4a46b7a2a3249d9cb4b8b1d485175
Dev Patel <devpatelm8318@gmail.com>
2023-05-20 23:44:11 +0530

    Update 404.js

--- pages/404.js
+++ pages/404.js
@@ -5,7 +5,7 @@ function ErrorPage() {
   return (
    <Layout>
     <div className='flex h-screen items-center justify-center'>
-       <div className=' text-4xl'> Work In Progress</div> 
+       <div className=' text-4xl'> Work In Progress...</div> 
     </div>
    </Layout>
   )
